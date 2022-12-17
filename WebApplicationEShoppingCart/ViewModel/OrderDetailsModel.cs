cae1ca8b1e6a3283ba3d530f91467c778531ea83
Oseni Mutiu <mutiu.oseni@fintraksoftware.com>
2022-12-17 13:28:02 +0100

    First Commit

---
+++ WebApplicationEShoppingCart/ViewModel/OrderDetailsModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationEShoppingCart.ViewModel
+{
+    public class OrderDetailsModel
+    {
+    }
+}
